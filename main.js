487cd03918f06f2fae5e51d076b3e92cc18610fb
nithin-hunt <nithinpsgice@gmail.com>
2022-12-31 14:29:20 +0530

    Add API to 5_day_forecast

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const API_KEY = "40d2cd7ef1adf0713734f1623aae50c3"
 
+const DAYS_OF_THE_WEEK = ["sun","mon","tue","wed","thu","fri","sat"];
+
 const getCurrentWeatherData = async() => {
     const city = "chennai";
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
@@ -41,6 +43,52 @@ const loadHourlyForecast = (hourlyForecast) => {
     hourlyContainer.innerHTML = innerHTMLString;
 };
 
+const calculateDayWiseForecast = (hourlyForecast) => {
+    let dayWiseForecast = new Map();
+    for (let forecast of hourlyForecast) {
+        const [date] = forecast.dt_txt.split(" ");
+        const dayOfTheWeek = DAYS_OF_THE_WEEK[new Date(date).getDay()];
+        console.log(dayOfTheWeek)
+        if(dayWiseForecast.has(dayOfTheWeek)) {
+            let forecastForTheDay = dayWiseForecast.get(dayOfTheWeek);
+            forecastForTheDay.push(forecast);
+            dayWiseForecast.set(dayOfTheWeek, forecastForTheDay);
+        } else {
+            dayWiseForecast.set(dayOfTheWeek, [forecast]);
+        }   
+    }
+
+    for(let [key,value] of dayWiseForecast) {
+        let temp_min = Math.min(...Array.from(value, val => val.temp_min));
+        let temp_max = Math.max(...Array.from(value, val => val.temp_max));
+        
+        dayWiseForecast.set(key, {temp_min, temp_max, icon: value.find(v => v.icon).icon})
+    }
+    console.log(dayWiseForecast)
+    return dayWiseForecast
+}
+
+const loadFiveDayForecast = (hourlyForecast) => {
+    console.log(hourlyForecast);
+    const dayWiseForecast = calculateDayWiseForecast(hourlyForecast);
+    const container = document.querySelector(".five-day-forecast-container");
+    let dayWiseInfo = "";
+    Array.from(dayWiseForecast).map(([day, {temp_max, temp_min, icon}], index) => {
+
+        if (index < 5) {
+            dayWiseInfo += 
+            `<article class="day-wise-forecast">
+                <h3 class="day">${ index === 0 ? "today": day}</h3>
+                <img src="${createIconUrl(icon)}" alt="weather-icon" class="icon" />
+                <p class="min-temp">${formatTemperature(temp_min)}</p>
+                <p class="max-temp">${formatTemperature(temp_max)}</p>
+            </article>`
+        }
+    });
+
+    container.innerHTML = dayWiseInfo;
+}
+
 const loadFeelsLike = ({main: {feels_like}}) => {
     let container = document.getElementById("feels-like");
     container.querySelector(".feels-like-temp").textContent = formatTemperature(feels_like);
@@ -55,6 +103,9 @@ document.addEventListener("DOMContentLoaded", async() => {
     loadCurrentForecast(currentWeather);
     const hourlyForecast = await getHourlyForecast(currentWeather);
     loadHourlyForecast(hourlyForecast);
+    loadFiveDayForecast(hourlyForecast);
     loadFeelsLike(currentWeather);
     loadHumidity(currentWeather);
+
+
 })
